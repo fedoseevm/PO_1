646e65410700d92acfe3acc3a33f9795ef4f8ec4
fedoseevm <maksim.fedoseev122@gmail.com>
2023-09-19 06:02:52 +0100

    Test

---
+++ project_2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;  // Obsluguje zapytania
+using System.Text;  // kodowanie znakow ASCII i symboli
+using System.Threading.Tasks; 
+
+namespace project_2
+{
+    internal class Program
+    {
+        static void Main(string[] args) // METODA glowna
+        {
+            Console.WriteLine("C#");
+            Console.ReadKey();
+        }
+    }
+}
