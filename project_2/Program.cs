2359093d2a070add0f7602387e2e7438f0629085
Fedoseev Maksim <maksim.fedoseev2022@technikum19.edu.pl>
2023-10-06 20:20:41 +0200

    male zmiany

--- project_2/Program.cs
+++ project_2/Program.cs
@@ -12,17 +12,20 @@ namespace project_2
         {
             Console.WriteLine("C#");
             Console.Clear(); // czyszczenie ekranu
-            //Console.ReadKey();  // Console - klasa
-                                // ReadKey() - metoda
-                                // KOMPILATOR - program do przemiany zwyklego tekstu do komend rozumianych przez program. Tlumaczy zwyklu kod programistyczny na kod maszynowy. C, C++, C#, Java
-                                // C# -> CIL -> kod posredni -> kod maszynowy
+                             //Console.ReadKey();  // Console - klasa
+                             // ReadKey() - metoda
+
+            // KOMPILATOR - program do przemiany zwyklego tekstu do komend rozumianych przez program. Tlumaczy zwykly kod programistyczny na kod maszynowy. C, C++, C#, Java
+            // C# -> CIL (Common Intermediate Language - Wspólny język pośredni) -> kod posredni -> kod maszynowy
 
             // INTERPRETER - program ktora czyta i analizuje kod programu i wykonuje go na biezaco. Python, JavaScript
             // kod -> interpreter
 
+
             // "C:\Users\2e1\2E MF\ProgObiektowe_1\project_2" - sciezka bezwzgledna
 
-            // zmienne - deklaracja i inicjalizacja
+            // Zmienne - deklaracja i inicjalizacja
+            Console.WriteLine("Zmienne - deklaracja i inicjalizacja\n");
             int i = 10; // 32 bity -> 4 bajty -> System.Int32
             // int: -2 147 483 648 - 2 147 483 647
             Console.WriteLine("Wartosc wynosi: " + i);      // + - Konkatynacja
@@ -34,28 +37,31 @@ namespace project_2
 
             byte b = 255;   // byte: 0-255
             sbyte sb = -128; // sbyte: -128 - 127
-
-            short s = -30000; // short: -32768 - 326767
+            short s = -30000; // short: -32768 - 32767
             //Console.ReadKey();
-
-            /*
+            /* ---------------------
              * byte 0-255 (8 bitow => 1 bajt)
              * sbyte -128 - 127 (8 bitow => 1 bajt)
+             * ---------------------
              * short -32 768 - 32 767 (16 bitow => 2 bajt)
+             * ushort 65 536 (16 bitow => 2 bajt)
+             * ---------------------
              * int -2 147 483 648 - 2 147 483 647 (32 bity => 4 bajty)
              * uint 0 - 4 294 967 295 (32 bity => 4 bajty)
+             * ---------------------
              * long -9 223 372 036 854 755 808 - 9 223 372 036 854 755 807 (64 bity => 8 bajtow) sufix L
              * ulong 0 - 18 446 744 073 709 551 615 (64 bity => 8 bajtow) sufix UL
-             * 
-             * bool (true or false) ()
-             * char U+0000 to U+FFFF (16 bitow => 2 bajty, znak z tablicy Unicode)
-             * string ?
+             * ---------------------
              * float -3.4 * 10^38 do 3.4 * 10^38 (32 bity) sufix F
              * double (64 bity)
              * decimal (128 bitow)
+             * ---------------------
+             * bool (true or false) (1 bajt)
+             * char U+0000 to U+FFFF (16 bitow => 2 bajty, znak z tablicy Unicode)
+             * string ?
              */
 
-            long l = -20L;
+            long l = -20;
             ulong ul = 20UL;
             Console.WriteLine("Zmienna l: {0}, zmienna ul: {1}", l, ul);
 
@@ -67,10 +73,14 @@ namespace project_2
 
 
             Console.WriteLine("Ilosc bajtow w pamieci typu float: " + sizeof(float));
-            Console.WriteLine("Ilosc bajtow w pamieci typu float: " + sizeof(int));
-            Console.WriteLine("Ilosc bajtow w pamieci typu float: " + sizeof(bool));
+            Console.WriteLine("Ilosc bajtow w pamieci typu int: " + sizeof(int));
+            Console.WriteLine("Ilosc bitow w pamieci typu int: " + Math.Pow(2, sizeof(int) * 8));
+            Console.WriteLine("Ilosc bajtow w pamieci typu bool: " + sizeof(bool));
+            Console.ReadKey();
+            Console.Clear();
 
             // zapis systemow liczbowych
+            Console.WriteLine("Zapis systemow liczbowych \n");
             // binarny
             Console.WriteLine(0b1010); // 10
 
@@ -93,19 +103,24 @@ namespace project_2
             Console.WriteLine(Int32.MinValue);        // -2 147 483 648
             Console.WriteLine(System.Int32.MinValue); // -2 147 483 648
 
-            Console.WriteLine(System.Int64.MinValue);
+            Console.WriteLine(System.Int64.MinValue); // -9 223 372 036 854 775 808
+            Console.WriteLine(System.Byte.MaxValue);
+
+            Console.ReadKey();
+            Console.Clear();
 
             // UNICODE
+            Console.WriteLine("UNICODE \n");
             Console.OutputEncoding = System.Text.Encoding.Unicode; // Rozwazanie problemu
             Console.WriteLine("♫");
             Console.WriteLine("\x266B");
 
             char letter = 'a';
             Console.WriteLine(letter);
-
+            
             char letter2 = (char)97;
             Console.WriteLine(letter2);
-            Console.WriteLine("\x1F60E");
+            Console.WriteLine("\ud83c\udca9"); // ????????????
             Console.ReadKey();
         }
     }
