092158d991b3b10cb5d145c4b1a5cabb5b55bfb0
fedoseevm <maksim.fedoseev122@gmail.com>
2023-12-12 09:30:52 +0100

    dodanie polecenia zadanie

--- Slowniki_project_1/Program.cs
+++ Slowniki_project_1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -139,6 +140,20 @@ namespace Slowniki_project_1
             {
                 Console.WriteLine("KLucz {0}, wartosc: {1}", item.Key, item.Value);
             }
+
+
+            // Napisz funkcję, która przyjmuje jako argument listę liczb całkowitych i zwraca słownik, w którym kluczem jest liczba, a wartością jest jej częstotliwość występowania na liście.
+            // Jeśli lista jest pusta lub null, funkcja powinna zwrócić pusty słownik.
+            // Przykład: dla listy[1, 2, 3, 2, 4, 1, 5, 2] funkcja powinna zwrócić słownik { 1: 2, 2: 3, 3: 1, 4: 1, 5: 1}.
+            // Napisz program, który wczytuje od użytkownika ciąg znaków, próbuje przekonwertować go na liczbę całkowitą za pomocą metody TryParse, a następnie wyświetla wynik na konsoli.
+            // Jeśli konwersja się powiedzie, program powinien wyświetlić liczbę i informację, że jest to poprawna liczba całkowita.
+            // Jeśli konwersja się nie powiedzie, program powinien wyświetlić informację, że podany ciąg znaków nie jest poprawną liczbą całkowitą.
+            // Przykład: dla ciągu znaków “123” program powinien wyświetlić “123 jest poprawną liczbą całkowitą”.
+            // Dla ciągu znaków “abc” program powinien wyświetlić “abc nie jest poprawną liczbą całkowitą”.
+
+
         }
+        // static public Dictionary<int, int> 
+        // ...
     }
 }
