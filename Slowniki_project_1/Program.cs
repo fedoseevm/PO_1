239c53cc0085dadaf1439a404b768178708c0f78
fedoseevm <maksim.fedoseev122@gmail.com>
2023-12-12 07:57:30 +0100

    add Slowniki_project_1

---
+++ Slowniki_project_1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slowniki_project_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> slownik = new Dictionary<int, string>();
+            slownik.Add(1, "Franek");
+            slownik.Add(2, "Anna");
+            slownik.Add(3, "Tomasz");
+
+            foreach (string key in slownik.Values) // var key in slownik
+            {
+                Console.WriteLine(key); // key.Value
+            }
+            Console.WriteLine();
+
+            // Tworzymy słownik, który przechowuje łańcuchy jako klucze i liczby jako wartości
+            Dictionary<string, int> slownik1 = new Dictionary<string, int>();
+
+            // Dodajemy kilka par klucz-wartość do słownika
+            slownik1.Add("jeden", 1);
+            slownik1.Add("dwa", 2);
+            slownik1.Add("trzy", 3);
+
+            // Wyświetlenie zawartości słownika za pomocą pętly foreach
+            foreach (KeyValuePair<string, int> pair in slownik1)    // Zamiast KeyValuePair<string, int> można użyć var
+            {
+                Console.WriteLine("Klucz: {0}, wartość: {1}", pair.Key, pair.Value);
+            }
+
+        }
+    }
+}
