cf18506e80980057c8b906158fe61c2f5b7b4d74
fedoseevm <maksim.fedoseev122@gmail.com>
2024-02-06 09:32:48 +0100

    add Kontruktor_1

---
+++ Konstruktory_1/Classes/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konstruktory_1.Classes
+{
+    internal class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Person(string name, string lastname)
+        {
+            FirstName = name;
+            LastName = lastname;
+        }
+    }
+}
---
+++ Konstruktory_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using Konstruktory_1.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konstruktory_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person nowak = new Person("Jan", "Nowak");
+        }
+    }
+}
