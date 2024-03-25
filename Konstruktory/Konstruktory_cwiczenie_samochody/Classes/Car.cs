65097b4d892cd833e5a8b4b8e732473204a3968d
Fedoseev Maksim <maksim.fedoseev2022@technikum19.edu.pl>
2024-03-25 19:53:57 +0100

    Add  Konstruktory_cwiczenie_samochody

---
+++ Konstruktory/Konstruktory_cwiczenie_samochody/Classes/Car.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Konstruktory_cwiczenie_samochody.Classes
+{
+    enum Type
+    {
+        Unknown,
+        Sedan,
+        SUV,
+        Coupe,
+        Sport
+    }
+
+    internal class Car
+    {
+        public string Model { get; set; }
+        public string Manufacturer { get; set; }
+        public DateTime ProductionDate { get; set; }
+        public int HorsePowers { get; set; }
+        public Type Type { get; set; }
+        public bool IsDiesel { get; set; }
+
+        public Car()
+        {
+            Model = "Unknown";
+            Manufacturer = "Unknown";
+            ProductionDate = DateTime.MinValue;
+            HorsePowers = 0;
+            Type = Type.Unknown;
+            IsDiesel = false;
+        }
+
+        public Car(string model)
+        {
+            Model = model;
+            Manufacturer = "Unknown";
+            ProductionDate = DateTime.MinValue;
+            HorsePowers = 0;
+            Type = Type.Unknown;
+            IsDiesel = false;
+        }
+
+        public Car(string model, string manufacturer) : this(model)
+        {
+            Manufacturer = manufacturer;
+        }
+
+        public Car(string model, string manufacturer, string productionDate) : this(model, manufacturer)
+        {
+            ProductionDate = DateTime.Parse(productionDate);
+        }
+
+        public Car(string model, string manufacturer, string productionDate, int horsePowers) : this(model, manufacturer, productionDate)
+        {
+            HorsePowers = horsePowers;
+        }
+
+        public Car(string model, string manufacturer, string productionDate, int horsePowers, string type) : this(model, manufacturer, productionDate, horsePowers)
+        {
+            if (type.ToUpper() == "SUV")
+                Type = (Type)Enum.Parse(typeof(Type), type.ToUpper());
+            else
+                Type = (Type)Enum.Parse(typeof(Type), CultureInfo.CurrentCulture.TextInfo.ToTitleCase(type));   
+            // Kapitalizacja podanego string'a zachowującego typ samochoda od razu przy tworzeniu obiekta klasy
+        }
+
+        public Car(string model, string manufacturer, string productionDate, int horsePowers, string type, bool isDiesel) : this(model, manufacturer, productionDate, horsePowers, type)
+        {
+            IsDiesel = isDiesel;
+        }
+    }
+}
---
+++ Konstruktory/Konstruktory_cwiczenie_samochody/Program.cs
@@ -0,0 +1,215 @@
+﻿using Konstruktory_cwiczenie_samochody.Classes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konstruktory_cwiczenie_samochody
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Car c1 = new Car("RX5", "Nissan", "2005-09-02", 512, "suv");
+            //Console.WriteLine(c1.Model + " " + c1.Manufacturer + " " + c1.ProductionDate + " " + c1.HorsePowers + " " + c1.Type);
+
+            List<Car> Cars = new List<Car>();
+            ShowMenu(Cars);
+        }
+
+        static void ShowMenu(List<Car> cars)
+        {
+            Console.Clear();
+
+            Console.WriteLine("Witaj w programie zarządzania samochodami!");
+            Console.WriteLine("Wybierz jedną z opcji: ");
+            Console.WriteLine("1. Dodaj samochód");
+            Console.WriteLine("2. Pokaż listę samochodów");
+            Console.WriteLine("3. Pokaż szczegóły samochoda");
+            Console.WriteLine("4. Usuń samochód z listy");
+            Console.WriteLine("5. Zakończ program");
+
+            Console.Write("\nWykonaj: ");
+            string input = Console.ReadLine();
+            switch (input)
+            {
+                case "1":
+                    AddNewCar(cars);
+                    break;
+                case "2":
+                    ShowCarList(cars);
+                    break;
+                case "3":
+                    ShowCarDetails(cars);
+                    break;
+                case "4":
+                    DeleteCar(cars);
+                    break;
+                case "5":
+                    Console.WriteLine("\nDziękujemy za korzystanie z programu!\n\n");
+                    break;
+                default:
+                    Console.WriteLine("Niepoprawna opcja. Naciśnij dowolny klawisz i spróbuj ponownie.");
+                    Console.ReadKey();
+                    ShowMenu(cars);
+                    break;
+            }
+        }
+
+        private static void AddNewCar(List<Car> cars)
+        {
+            Console.Clear();
+
+            try
+            {
+                Console.Write("Podaj model samochodu: ");
+                string model = Console.ReadLine();
+
+                Console.Write("Podaj producenta samochodu: ");
+                string manufacturer = Console.ReadLine();
+
+                Console.Write("Podaj datę produkcji samochodu w formacie YYYY-MM-DD: ");
+                string productionDate = Console.ReadLine();
+
+                Console.Write("Podaj rodzaj samochodu (Sedan, SUV, Coupe, Sport): ");
+                string type = Console.ReadLine();
+
+                Console.Write("Czy samochód ma silnik diesla (tak/nie): ");
+                bool isDiesel = Console.ReadLine().ToLower() == "tak";
+
+                Console.Write("Ile samochód ma koni: ");
+                int horsePowers = int.Parse(Console.ReadLine());
+
+                Car samochod = new Car(model, manufacturer, productionDate, horsePowers, type, isDiesel);
+                cars.Add(samochod);
+
+                Console.WriteLine("\nDodano nowy samochód: {0} {1} {2}", samochod.Manufacturer, samochod.Model, samochod.ProductionDate.Year);
+                Console.WriteLine("\nNaciśnij dowolny klawisz, aby wrócić do menu głównego.");
+                Console.ReadKey();
+                ShowMenu(cars);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("\nBłędne dane, spróbój ponownie. \nNaciśnij dowolny klawisz, aby wrócić do menu głównego.");
+                Console.ReadKey();
+                ShowMenu(cars);
+            }
+        }
+        private static void GetCarList(List<Car> cars)
+        {
+            Console.WriteLine("Lista samochodów: ");
+            for (int i = 0; i < cars.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {cars[i].Manufacturer} {cars[i].Model} {cars[i].ProductionDate.Year}");
+            }
+        }
+        private static void ShowCarList(List<Car> cars)
+        {
+            Console.Clear();
+
+            if (cars.Count == 0)
+            {
+                Console.WriteLine("Nie dodano żadnego samochodu.");
+            }
+            else
+            {
+                GetCarList(cars);
+
+            }
+
+            Console.WriteLine("\nNaciśnij dowolny klawisz, aby wrócić do menu głównego.");
+            Console.ReadKey();
+            ShowMenu(cars);
+        }
+
+        private static void ShowCarDetails(List<Car> cars)
+        {
+            Console.Clear();
+
+            if (cars.Count == 0)
+            {
+                Console.WriteLine("Nie dodano żadniego samochodu.");
+            }
+            else
+            {
+                GetCarList(cars);
+
+                Console.Write("\nPodaj numer samochodu, którego szczegóły chcesz zobaczyć: ");
+                int index = int.Parse(Console.ReadLine()) - 1;
+
+                if (index >= 0 && index < cars.Count)
+                {
+                    Car samochod = cars[index];
+                    Console.WriteLine("\nModel: {0}", samochod.Model);
+                    Console.WriteLine("Producent: {0}", samochod.Manufacturer);
+                    Console.WriteLine("Rodzaj: {0}", samochod.Type);
+                    Console.WriteLine("Data produkcji: {0}", samochod.ProductionDate.ToShortDateString());
+                    Console.WriteLine("Ilosc koni: {0}", samochod.HorsePowers);
+                    Console.WriteLine("Silnik diesla: {0}", samochod.IsDiesel ? "tak" : "nie");
+                }
+                else
+                {
+                    Console.WriteLine("Niepoprawny numer. Spróbuj ponownie.");
+                }
+            }
+
+            Console.WriteLine("\nNaciśnij dowolny klawisz, aby wrócić do menu głównego.");
+            Console.ReadKey();
+            ShowMenu(cars);
+        }
+
+        private static void DeleteCar(List<Car> cars)
+        {
+            Console.Clear();
+            if (cars.Count == 0)
+            {
+                Console.WriteLine("Nie dodano żadniego samochodu.");
+            }
+            else
+            {
+                Console.WriteLine("Wybierz jedną z opcji:");
+                Console.WriteLine("1. Usunąc jeden samochód z listy");
+                Console.WriteLine("2. Usunąc wszystkie samochody");
+
+                try
+                {
+                    Console.Write("\nWybór: ");
+                    string input = Console.ReadLine();
+                    switch (input)
+                    {
+                        case "1":
+                            Console.Clear();
+                            GetCarList(cars);
+                            Console.Write("\nPodaj numer samochoda, którego chcesz usunąć: ");
+                            int index = int.Parse(Console.ReadLine()) - 1;
+                            
+                            Console.Clear();
+                            Console.WriteLine($"Usunięto samochód o numerze {index + 1}: {cars[index].Manufacturer} {cars[index].Model} {cars[index].ProductionDate.Year}");
+                            cars.RemoveAt(index);
+                            break;
+                        case "2":
+                            cars.Clear();
+                            Console.WriteLine("\nUsunięto wszystkie samochody z listy.");
+                            break;
+                        default:
+                            Console.WriteLine("\nPodana niepoprawna wartość.");
+                            break;
+                    }
+
+
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("\nBłędne dane, spróbój ponownie.");
+                }
+
+                Console.WriteLine("\nNaciśnij dowolny klawisz, aby wrócić do menu głównego.");
+                Console.ReadKey();
+                ShowMenu(cars);
+
+            }
+        }
+    }
+}
